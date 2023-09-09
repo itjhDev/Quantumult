8b18e2c0020a1b28f0751ab6eb9dc9a5ca3c9f9a
LijunSong <github@itjh.com.cn>
2023-09-09 21:55:08 +0800

    Add files via upload

---
+++ Scripts/nicegramvip2023.js
@@ -0,0 +1,22 @@
+/*************************************
+
+项目名称：Nicegram
+下载地址：https://t.cn/A6ou0MCe
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+
+
+[rewrite_local]
+
+
+https?:\/\/my\.nicegram\.app\/api\/api\/v4\/settings\\d{5,10} url echo-response text/json echo-response nicegramvip2023.js
+[mitm]
+hostname = my.nicegram.app
+*************************************/
+
+
+{"premium": true};
